2c597a95e90b0ac2df8ee3b035fd1a11d3cbb59c
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2022-03-26 13:49:15 -0600

    Get info for all ato plays

--- extract_info.R
+++ extract_info.R
@@ -1,18 +1,25 @@
-games <- readRDS("pbp_data.RDS")
-games[[1]]
+games <- readRDS("pbp8.RDS")
+
+# 5007, 6493, 8504
 library(listviewer)
-jsonedit(games[[288]])
-all_events <-games[[288]]$periods[[1]]$events
-event_type <- c()
-for(i in 1:length(all_events)){
-    event_type[i] <- all_events[[i]]$event_type
-}
-length(events)
+library(progress)
+jsonedit(games[[1]])
 library(lubridate)
 library(tidyverse)
 test <- data.frame()
+pb <- progress_bar$new(format = "  downloading [:bar] :percent eta: :eta",
+                       total = length(games))
 for(game in 1:length(games)){
     if(length(games[[game]]) == 1){
+        next
+    }
+    if(games[[game]]$status %in% c("postponed", "cancelled")){
+        next
+    }
+    if(length(games[[game]]$periods[[1]]$events) < 1){
+        next
+    }
+    if(game %in% c(5007, 6493, 8504)){
         next
     }
     for(half in 1:length(games[[game]]$periods)){
@@ -20,10 +27,31 @@ for(game in 1:length(games)){
         event_types <- c()
         timeouts <- c()
         timeouts_possessions <- c()
-        if (length(events) == 0){
-            next
+        possession_ids <- c()
+        home_team <- games[[game]]$home$market
+        away_team <- games[[game]]$away$market
+        home_basket <- c()
+        away_basket <- c()
+        if(events[[1]]$attribution$market == home_team){
+            home_basket <- events[[1]]$attribution$team_basket
+        } else {
+            away_basket <- events[[1]]$attribution$team_basket
         }
-        
+        if(is.null(home_basket)){
+            if(away_basket == "right"){
+                home_basket <- "left"
+            }
+            else {
+                home_basket <- "right"
+            }
+        }else {
+            if(home_basket == "right"){
+                away_basket <- "left"
+            } 
+            else {
+                away_basket <- "right"
+            }
+        }
         for(i in 1:length(events)){
             event_types <- c(event_types, events[[i]]$event_type)
             if(events[[i]]$event_type %in% c("teamtimeout", "tvtimeout", "officialtimeout")){
@@ -31,6 +59,7 @@ for(game in 1:length(games)){
                     timeouts <- c(timeouts, i)
                     team_possession <- events[[i]]$possession$market
                     timeouts_possessions <- c(timeouts_possessions, team_possession)
+                    possession_ids <- c(possession_ids, events[[i]]$possession$id)
                 }
                 else{
                     next
@@ -43,6 +72,7 @@ for(game in 1:length(games)){
             possession_x_location <- numeric()
             possession_y_location <- numeric()
             score_diff <- numeric()
+            team_basket <- c()
             time <- c()
             for(i in 1:length(timeouts)){
                 event_counter <- timeouts[i]
@@ -72,17 +102,29 @@ for(game in 1:length(games)){
                     possession_y_location[i] <- events[[event_counter - 1]]$location$coord_y   
                 }
                 possession_score[i] <- points
+                if(timeouts_possessions[i] == home_team){
+                    team_basket[i] <- home_basket
+                } else {
+                    team_basket[i] <- away_basket
+                }
                 if(half == 1){
                     time[i] <- as.numeric(ms(events[[event_counter - 1]]$clock) + minutes(20))
                 }
                 time[i] <- as.numeric(ms(events[[event_counter - 1]]$clock))
             }
-            test.half <- data.frame(possession_score, possession_x_location, possession_y_location, score_diff, time, timeouts_possessions)
+            game_date <- rep(games[[game]]$scheduled, length(timeouts_possessions))
+            test.half <- data.frame(possession_score, possession_x_location, possession_y_location, 
+                                    score_diff, time, timeouts_possessions, possession_ids, game_date,
+                                    team_basket, half = rep(half, length(timeouts_possessions)), 
+                                    game_number = rep(game, length(timeouts_possessions)))
             test <- test %>% bind_rows(test.half)
         }
         else{
             next
         }
     }
+    pb$tick()
 }    
 
+
+write.csv(test, "ato.csv")
