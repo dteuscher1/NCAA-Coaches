fd701916c731da0f42022b3bb63f156d2ab967df
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2022-03-31 09:52:52 -0600

    Additional model work

--- models.R
+++ models.R
@@ -15,7 +15,7 @@ lm_model <- lm(possession_score ~ possession_x_location + possession_y_location 
 library(ranger)
 rf <- ranger(possession_score ~ possession_x_location + possession_y_location + score_diff + time + team_basket, data = plays)
 
-obs <- sample(nrow(plays) *.3, 1:nrow(plays), replace = FALSE)
+obs <- sample(1:nrow(plays), nrow(plays) *.3, replace = FALSE)
 train <- plays[-obs,]
 test <- plays[obs, ]
 lm_train <-  lm(possession_score ~ possession_x_location + possession_y_location + score_diff + time + team_basket, data = train)
@@ -26,12 +26,12 @@ rmse <- (rf_preds$predictions - test$possession_score)^2 %>% sqrt() %>% mean()
 rmse2 <- (lm_preds - test$possession_score)^2 %>% sqrt() %>% mean()
 attempt <- plays %>% 
     mutate(pred = rf$predictions) %>% 
-    group_by(game_coach) %>%
+    group_by(timeouts_possessions, game_coach) %>%
     summarize(expected = mean(pred),
               actual = mean(possession_score), 
               diff = actual - expected, 
-              n = n()) %>%
-    inner_join(coach_info %>% dplyr::select(Coach, team_market), by = c('game_coach' = 'Coach'))
+              n = n()) #%>%
+    #inner_join(coach_info %>% dplyr::select(Coach, team_market), by = c('game_coach' = 'Coach'))
 coach_info <- read.csv("coaches_data.csv")
 
 attempt %>% arrange(desc(diff)) %>% filter(n > 100)
@@ -39,3 +39,22 @@ attempt %>% arrange(diff) %>% filter(n > 100)
 
 plot(attempt$expected, attempt$diff)
 
+library(modelr)
+plays_grid <- plays %>% 
+    data_grid(possession_x_location = seq(min(possession_x_location), 450, length = 40),
+              possession_y_location = seq(min(possession_y_location), max(possession_y_location), length = 40),
+              score_diff = seq(-25, 25, by = 5), 
+              time = seq(max(plays$time), min(plays$time), length = 15), 
+              team_basket = "left")
+library(broom)
+grid_hat <- predict(rf, data = plays_grid)
+
+ggplot(plays_grid, aes(x = possession_x_location, y = possession_y_location)) + 
+    geom_tile(aes(fill = grid_hat$predictions), alpha = 0.7) + 
+    scale_fill_gradient("Expected Points Per Possession", breaks = c(.5, 1, 1.5, 2)) +
+    theme_minimal()
+
+right <- plays %>% filter(team_basket == "left")
+plot(right$possession_x_location, right$possession_y_location)
+
+grid_hat$predictions
