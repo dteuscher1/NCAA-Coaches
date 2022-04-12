922c375c0025c98e480eacdd76339cec9765f243
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2022-04-12 12:46:03 -0600

    Add in season variable

--- create_model_data.R
+++ create_model_data.R
@@ -1,11 +1,32 @@
-plays <- read.csv("ato.csv")
-coaches <- read.csv("coaches_data.csv")
+## Author: David Teuscher
+## Last Edited: 04.12.22
+## This script takes the coach information for all the selected teams
+## and extracts the coaches who coached from 2013-2022. The start and end dates
+## for the coaches are included as well. 
+##############################################################################
+
+# Load packages
 library(tidyverse)
-glimpse(plays)
+
+# Load plays dataset and coaches data
+plays <- read.csv("ato_v2.csv")
+coaches <- read.csv("coaches_data.csv")
+
+# Change the date to a date type and then create a season variable
 plays <- plays %>% 
     mutate(game_date = as.Date(game_date), 
-           timeouts_possessions = str_replace(timeouts_possessions, "BYU", "Brigham Young"))
+           timeouts_possessions = str_replace(timeouts_possessions, "BYU", "Brigham Young"), 
+           season = ifelse(between(game_date, as.Date("2013-11-08"), as.Date("2014-04-07")), 2013,
+                           ifelse(between(game_date, as.Date("2014-11-14"), as.Date("2015-04-06")), 2014,
+                                  ifelse(between(game_date, as.Date("2015-11-13"), as.Date("2016-04-04")), 2015,
+                                         ifelse(between(game_date, as.Date("2016-11-11"), as.Date("2017-04-03")), 2016,
+                                                ifelse(between(game_date, as.Date("2017-11-10"), as.Date("2018-04-02")), 2017,
+                                                       ifelse(between(game_date, as.Date("2018-11-06"), as.Date("2019-04-08")), 2018,
+                                                              ifelse(between(game_date, as.Date("2019-11-05"), as.Date("2020-04-06")), 2019,
+                                                                     ifelse(between(game_date, as.Date("2020-11-25"), as.Date("2021-04-05")), 2020,
+                                                                            ifelse(between(game_date, as.Date("2021-11-09"), as.Date("2022-04-04")), 2021, NA))))))))))
 
+# Extract the coach of a team for each game
 game_coach <- c()
 for(i in 1:nrow(plays)){
     coach <- coaches[which(coaches$team_market %in% plays$timeouts_possessions[i]), ]
@@ -15,9 +36,10 @@ for(i in 1:nrow(plays)){
         }
     }
 }
-unique(plays$timeouts_possessions) %>% sort()
 
-
+# Bind the team coach to the dataset
 model_data <- plays %>% 
     mutate(game_coach = game_coach)
-write.csv(model_data, "model_data.csv", row.names = FALSE)
+
+# Write the data to a .csv file
+write.csv(model_data, "model_data_v2.csv", row.names = FALSE)
