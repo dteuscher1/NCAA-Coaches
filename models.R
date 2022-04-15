dd3d7dcf928065e95549b5c38b365b9a5aa40c45
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2022-04-15 17:41:01 -0600

    Work on model

--- models.R
+++ models.R
@@ -1,32 +1,93 @@
-plays <- read.csv("model_data.csv") %>%
+## Author: David Teuscher
+## Last Edited: 04.12.22
+## This script provides the code for the Shiny app presenting the results
+## of this analysis
+##############################################################################
+
+# Load packages
+library(DataExplorer)
+library(tidyverse)
+
+plays <- read.csv("model_data_v2.csv") 
+
+# %>%
+#     dplyr::select(-X, -game_number, -possession_ids, -half) %>%
+#     mutate(team_basket = factor(team_basket)) %>%
+#     filter(!is.na(possession_x_location)) %>%
+#     filter(!is.na(time)) %>%
+#     filter(!is.na(game_coach))
+
+plot_missing(plays)
+
+library(tidymodels)
+plot_missing(plays)
+
+set.seed(222)
+# Put 3/4 of the data into the training set 
+data_split <- initial_split(plays2, prop = 3/4)
+
+# Create data frames for the two sets:
+train_data <- training(data_split)
+test_data  <- testing(data_split)
+
+
+plays_rec <- recipe(possession_score ~ possession_x_location + possession_y_location + score_diff + time + team_basket, data = train_data)
+
+cores <- parallel::detectCores()
+
+rf_mod <- rand_forest(trees = 1000) %>%
+    set_engine("ranger", num.threads = cores) %>%
+    set_mode("regression")
+
+plays_wflow <- workflow() %>% 
+    add_model(rf_mod) %>% 
+    add_recipe(plays_rec)
+
+plays_fit <- plays_wflow %>% 
+    fit(data = train_data)
+
+folds <- vfold_cv(train_data, v = 10)
+folds
+
+plays_fit_rs <- plays_wflow %>% 
+    fit_resamples(folds)
+
+collect_metrics(plays_fit_rs)
+
+plays_testing_pred <- predict(plays_fit, plays2) %>% 
+    bind_cols(plays2 %>% select(possession_score))
+
+plays_testing_pred %>% 
+    rmse(possession_score, .pred)
+
+plays2 <- plays %>% 
     dplyr::select(-X, -game_number, -possession_ids, -half) %>%
     mutate(team_basket = factor(team_basket)) %>%
     filter(!is.na(possession_x_location)) %>%
-    filter(!is.na(time)) %>%
-    filter(!is.na(game_coach))
+    filter(!is.na(season)) %>%
+    filter(!is.na(game_coach)) %>%
+    filter(!is.na(time)) %>% 
+    filter((team_basket == "right" & possession_x_location > 550) | (team_basket == "left" & possession_x_location < 450)) %>%
+    filter(possession_score <= 3)
 
-library(DataExplorer)
-library(glmnet)
-plot_missing(plays)
-library(tidyverse)
-glimpse(plays)
+plot_missing(plays2)
 
 lm_model <- lm(possession_score ~ possession_x_location + possession_y_location + score_diff + time + team_basket, data = plays)
 library(ranger)
 rf <- ranger(possession_score ~ possession_x_location + possession_y_location + score_diff + time + team_basket, data = plays)
 
-obs <- sample(1:nrow(plays), nrow(plays) *.3, replace = FALSE)
-train <- plays[-obs,]
-test <- plays[obs, ]
+obs <- sample(1:nrow(plays2), nrow(plays2) *.3, replace = FALSE)
+train <- plays2[-obs,]
+test <- plays2[obs, ]
 lm_train <-  lm(possession_score ~ possession_x_location + possession_y_location + score_diff + time + team_basket, data = train)
 model <- ranger(possession_score ~ possession_x_location + possession_y_location + score_diff + time + team_basket, data = train)
 rf_preds <- predict(model, test)
 lm_preds <- predict(lm_train, test)
 rmse <- (rf_preds$predictions - test$possession_score)^2 %>% sqrt() %>% mean()
 rmse2 <- (lm_preds - test$possession_score)^2 %>% sqrt() %>% mean()
-attempt <- plays %>% 
-    mutate(pred = rf$predictions) %>% 
-    group_by(timeouts_possessions, game_coach) %>%
+attempt <- plays2 %>% 
+    mutate(pred = plays_testing_pred$.pred) %>% 
+    group_by(timeouts_possessions) %>%
     summarize(expected = mean(pred),
               actual = mean(possession_score), 
               diff = actual - expected, 
@@ -54,7 +115,7 @@ ggplot(plays_grid, aes(x = possession_x_location, y = possession_y_location)) +
     scale_fill_gradient("Expected Points Per Possession", breaks = c(.5, 1, 1.5, 2)) +
     theme_minimal()
 
-right <- plays %>% filter(team_basket == "left")
+right <- plays2 %>% filter(team_basket == "right")
 plot(right$possession_x_location, right$possession_y_location)
 
 grid_hat$predictions
