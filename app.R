a47879184b7848a18d29be50561d334c123ad225
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2022-04-16 22:44:46 -0600

    Finalize Shiny app

--- app.R
+++ app.R
@@ -15,7 +15,13 @@ library(cfbplotR)
 library(tidyverse)
 
 # Run the script that is needed for the app
-source("global.R")
+team_info <- read.csv("team_ids.csv")
+
+plays <- read.csv("shiny_data.csv") %>%
+    rename(Team = timeouts_possessions,
+           Coach = game_coach, 
+           Season = season) %>%
+    mutate(Team = ifelse(Team == "Brigham Young", "BYU", Team)) 
 
 # Set up UI
 ui <- dashboardPage(
@@ -56,7 +62,9 @@ ui <- dashboardPage(
                             column(width = 6,
                                    # Provide option to select the minimum number of plays a team/coach had
                                    selectizeInput('team2', 'Choose number of minimum plays', 1:100, 100),
-                                   selectizeInput('group', "Chose variables to group by", c("Team", "Coach", "Season"), selected = c("Team"), multiple = TRUE),
+                                   p("If grouping by another variable, remove the none option."),
+                                   # Provide options for grouping
+                                   selectizeInput('group', "Choose variables to group by", c("None", "Coach", "Season"), selected = c("None"), multiple = TRUE),
                                    actionButton('update2', 'Update')
                             )
                         )
@@ -73,6 +81,8 @@ ui <- dashboardPage(
                     h1(strong("Expected vs. Acutal Points")), 
                     fluidRow(
                         box(
+                            # Options to for grouping, highlighting a conference, filtering for a season
+                            # and determining the number of minimum plays
                             selectizeInput('group3', "Choose variables to group by", c("Team", "Coach"), selected = c("Team"), multiple = TRUE),
                             selectizeInput('conf', "Choose a conference to highlight ", c("None", unique(team_info$conference)), selected = c("None")),
                             selectizeInput('season', "Choose a specific season ", c("None", unique(plays$Season)), selected = c("None")),
@@ -80,6 +90,7 @@ ui <- dashboardPage(
                             actionButton('update1', 'Update')
                         )    
                     ),
+                    # Print warning about plots taking a little bit to appear
                     h6("Note: The plot takes a number of seconds to appear."),
                     # Display the plot
                     fluidRow(
@@ -95,8 +106,10 @@ ui <- dashboardPage(
                             column(width = 6,
                                    # Provide option to select the minimum number of plays a team/coach had
                                    selectizeInput('plays', 'Choose number of minimum plays', 1:100, 100),
+                                   # Option to group by variables
                                    selectizeInput('group2', "Choose variables to group by", c("Team", "Coach", "Season"), selected = c("Team"), multiple = TRUE),
                                    actionButton('update3', 'Update'), 
+                                   # Option to download data
                                    downloadButton("downloadData", "Download")
                             )
                         )
@@ -116,43 +129,91 @@ ui <- dashboardPage(
 server <- function(input, output, session){
     # Reactive function to create table
     rplot_selected <- eventReactive(input$update2, {
-        # per possession and the number of plays out of a timeout
-        attempt <- plays %>% 
-            group_by_at(input$group) %>%
-            summarize(expected = round(mean(RF), digits = 3),
-                      actual = round(mean(possession_score), digits = 3), 
-                      diff = round(actual - expected, digits = 3), 
-                      n = n()) %>%
-            arrange(desc(diff))
-        
-        # Join the data with the team data and change some teams and conferences to match the 
-        # strings in the cfbplotR package
-        attempt2 <- attempt %>% inner_join(team_info, by = c("Team" = "market")) %>%
-            mutate(Team = ifelse(Team == "North Carolina State", "NC State", Team),
-                   Team = ifelse(Team == "Miami (FL)", "Miami", Team)) %>%
-            dplyr::select(-X, -id, -alias, -conf_alias) %>% 
-            mutate(Conference_logo = ifelse(conference == "West Coast", "NCAA", conference),
-                   Conference_logo = ifelse(Conference_logo == "Big East", "NCAA", Conference_logo),
-                   Conference_logo = ifelse(Conference_logo == "Southeastern", "SEC", Conference_logo),
-                   Conference_logo = ifelse(Conference_logo == "Atlantic Coast", "ACC", Conference_logo),
-                   Conference_logo = ifelse(Conference_logo == "Pacific 12", "Pac-12", Conference_logo),
-                   Team_logo = Team) %>%
-            dplyr::select_at(c(input$group, "name", "Team_logo", "expected", "actual", "diff", "n", "conference", "Conference_logo"))
-        
-        # Rename the columns of the data frame
-        names(attempt2) <- c(c(input$group), "Team Name", "Logo", "Expected Points", "Actual Points", "Difference", "Number of Plays", "Conference", "Conference Logo")
-        # Filter table by the number of plays
-        displayTable <- attempt2 %>% 
-            filter(`Number of Plays` > as.numeric(input$team2)) %>%
-            gt() %>% 
-            # Format the team and conference logo
-            gt_fmt_cfb_logo(columns = c(`Conference Logo`, `Logo`)) #%>%
-            #gt_merge_stack_team_color(Team,`Team Name`,Team)
-        displayTable
+        # If only grouping by team, create table
+        if(length(input$group) == 1 & input$group == "None"){
+            # Calculate expected and actual points per possession by Team
+            # Arrange in descending order
+            attempt <- plays %>% 
+                group_by_at("Team") %>%
+                summarize(expected = round(mean(RF), digits = 3),
+                          actual = round(mean(possession_score), digits = 3), 
+                          diff = round(actual - expected, digits = 3), 
+                          n = n()) %>%
+                arrange(desc(diff))
+            
+            # Join the data with the team data and change some teams and conferences to match the 
+            # strings in the cfbplotR package
+            attempt2 <- attempt %>% inner_join(team_info, by = c("Team" = "market")) %>%
+                mutate(Team = ifelse(Team == "North Carolina State", "NC State", Team),
+                       Team = ifelse(Team == "Miami (FL)", "Miami", Team)) %>%
+                dplyr::select(-X, -id, -alias, -conf_alias) %>% 
+                mutate(Conference_logo = ifelse(conference == "West Coast", "NCAA", conference),
+                       Conference_logo = ifelse(Conference_logo == "Big East", "NCAA", Conference_logo),
+                       Conference_logo = ifelse(Conference_logo == "Southeastern", "SEC", Conference_logo),
+                       Conference_logo = ifelse(Conference_logo == "Atlantic Coast", "ACC", Conference_logo),
+                       Conference_logo = ifelse(Conference_logo == "Pacific 12", "Pac-12", Conference_logo),
+                       Team_logo = Team) %>%
+                dplyr::select_at(c("Team", "name", "Team_logo", "expected", "actual", "diff", "n", "conference", "Conference_logo"))
+            
+            # Rename the columns of the data frame
+            names(attempt2) <- c(c("Team"), "Team Name", "Logo", "Expected Points", "Actual Points", "Difference", "Number of Plays", "Conference", "Conference Logo")
+            # Filter table by the number of plays
+            displayTable <- attempt2 %>% 
+                filter(`Number of Plays` > as.numeric(input$team2)) %>%
+                gt() %>% 
+                # Format the team and conference logo
+                gt_fmt_cfb_logo(columns = c(`Conference Logo`, `Logo`)) %>%
+                gt_merge_stack_team_color(Team,`Team Name`,Team)
+            displayTable
+        } else {
+            #  Calculate expected and actual points per possession by Team and other grouping variables
+            # Arrange in descending order
+            attempt <- plays %>% 
+                group_by_at(c("Team",input$group)) %>%
+                summarize(expected = round(mean(RF), digits = 3),
+                          actual = round(mean(possession_score), digits = 3), 
+                          diff = round(actual - expected, digits = 3), 
+                          n = n()) %>%
+                arrange(desc(diff))
+            
+            # Join the data with the team data and change some teams and conferences to match the 
+            # strings in the cfbplotR package
+            attempt2 <- attempt %>% inner_join(team_info, by = c("Team" = "market")) %>%
+                mutate(Team = ifelse(Team == "North Carolina State", "NC State", Team),
+                       Team = ifelse(Team == "Miami (FL)", "Miami", Team)) %>%
+                dplyr::select(-X, -id, -alias, -conf_alias) %>% 
+                mutate(Conference_logo = ifelse(conference == "West Coast", "NCAA", conference),
+                       Conference_logo = ifelse(Conference_logo == "Big East", "NCAA", Conference_logo),
+                       Conference_logo = ifelse(Conference_logo == "Southeastern", "SEC", Conference_logo),
+                       Conference_logo = ifelse(Conference_logo == "Atlantic Coast", "ACC", Conference_logo),
+                       Conference_logo = ifelse(Conference_logo == "Pacific 12", "Pac-12", Conference_logo),
+                       Team_logo = Team) %>%
+                dplyr::select_at(c("Team", input$group, "name", "Team_logo", "expected", "actual", "diff", "n", "conference", "Conference_logo"))
+            
+            # Rename the columns of the data frame
+            names(attempt2) <- c(c("Team", input$group), "Team Name", "Logo", "Expected Points", "Actual Points", "Difference", "Number of Plays", "Conference", "Conference Logo")
+            # Filter table by the number of plays
+            displayTable <- attempt2 %>% 
+                filter(`Number of Plays` > as.numeric(input$team2)) %>%
+                gt() %>% 
+                # Set summary rows for each group
+                summary_rows(groups = TRUE, 
+                             columns = c(`Expected Points`, `Actual Points`, `Difference`, `Number of Plays`), 
+                             fns = list(Average = ~mean(.),
+                                        Minimum = ~min(.),
+                                        Maximum = ~max(.))) %>% 
+                # Format the team and conference logo
+                gt_fmt_cfb_logo(columns = c(`Conference Logo`, `Logo`)) %>%
+                gt_merge_stack_team_color(Team,`Team Name`,Team)
+            displayTable
+        }    
     })
-    # Reactive function to draw acutal vs. expected plot
+    # Reactive function to draw actual vs. expected plot
     rplot_resid <- eventReactive(input$update1, {
+        # If selected a season, plot data from all seasons
         if(input$season != "None"){
+            # Calculate expected and actual points per possession by grouping variables and season
+            # Arrange in descending order
             attempt <- plays %>% 
                 group_by_at(c(input$group3, "Season")) %>%
                 summarize(expected = round(mean(RF), digits = 3),
@@ -160,8 +221,12 @@ server <- function(input, output, session){
                           diff = round(actual - expected, digits = 3), 
                           n = n()) %>%
                 arrange(desc(diff)) %>%
+                # Filter for season and minimum number of plays
                 filter(Season == as.numeric(input$season), n > as.numeric(input$minplays))
         } else {
+            # Calculate expected and actual points per possession by grouping vars
+            # Arrange in descending order
+            # Filter for minimum number of plays
             attempt <- plays %>% 
                 group_by_at(input$group3) %>%
                 summarize(expected = round(mean(RF), digits = 3),
@@ -185,11 +250,15 @@ server <- function(input, output, session){
                    Conference_logo = ifelse(Conference_logo == "Pacific 12", "Pac-12", Conference_logo),
                    Team_logo = Team) %>%
             dplyr::select_at(c(input$group3, "name", "Team_logo", "expected", "actual", "diff", "n", "conference", "Conference_logo"))
+        # Rename the columns of the data frame
         names(attempt2) <- c(c(input$group3), "Team Name", "Logo", "Expected Points", "Actual Points", "Difference", "Number of Plays", "Conference", "Conference Logo")
+        # If a conference is selected, create the plot that highlights the conference
         if(input$conf != "None"){
             attempt2 <- attempt2 %>%
+                # Set the color for the conference
                 mutate(color = if_else(Conference == input$conf,NA_character_,"b/w"),
                        alpha = if_else(Conference == input$conf,1,.4))
+            # Create the plot
             plot <- ggplot(attempt2, aes(x = `Expected Points`, y = `Actual Points`)) +
                 geom_median_lines(aes(v_var = `Expected Points`, h_var = `Actual Points`)) +
                 geom_cfb_logos(aes(team = Team, alpha = alpha, color = color), width = 0.025) +
@@ -200,7 +269,7 @@ server <- function(input, output, session){
                 labs(title = "Expected vs. Actual Points per Possession", subtitle = "Better teams are in the top right quadrant and worse teams in the bottom left quadrant")
             plot
         } else {
-        # Rename the columns of the data frame
+        # Create the plot
             plot <- ggplot(attempt2, aes(x = `Expected Points`, y = `Actual Points`)) +
                 geom_median_lines(aes(v_var = `Expected Points`, h_var = `Actual Points`)) +
                 geom_cfb_logos(aes(team = Team), width = 0.025) +
@@ -212,6 +281,8 @@ server <- function(input, output, session){
     })
     
     rsort <- eventReactive(input$update3, {
+        # Calculate expected and actual points per possession by grouping variables and season
+        # Arrange in descending order
         attempt <- plays %>% 
             group_by_at(input$group2) %>%
             summarize(expected = round(mean(RF), digits = 3),
@@ -220,6 +291,7 @@ server <- function(input, output, session){
                       n = n()) %>%
             filter(n > as.numeric(input$plays)) %>%
             arrange(desc(diff))
+        # Rename the columns of the data frame
         names(attempt) <- c(input$group2, "Expected Points", "Actual Points", "Difference", "Number of Plays")
         DisplayTable <- attempt
     })
--- models.R
+++ models.R
@@ -9,13 +9,13 @@ library(DataExplorer)
 library(tidyverse)
 library(tidymodels)
 
+# Load in data
 plays <- read.csv("model_data_v2.csv") 
 
-
+# Check missing values
 plot_missing(plays)
 
-
-
+# Remove missing value and columns not needed 
 plays <- plays %>%
     dplyr::select(-X, -game_number, -possession_ids, -half) %>%
     mutate(team_basket = factor(team_basket)) %>%
@@ -26,9 +26,14 @@ plays <- plays %>%
     filter((team_basket == "right" & possession_x_location > 550) | (team_basket == "left" & possession_x_location < 450)) %>%
     filter(possession_score <= 3)
 
+# Check missing values again
 plot_missing(plays)
+
+# Set seed
 set.seed(222)
+# Take small sample of plays to train the model
 plays_small <- plays %>% sample_n(15000)
+
 # Put 3/4 of the data into the training set 
 data_split <- initial_split(plays_small, prop = 3/4)
 
@@ -36,18 +41,21 @@ data_split <- initial_split(plays_small, prop = 3/4)
 train_data <- training(data_split)
 test_data  <- testing(data_split)
 
-
+# Create a recipe for the model
 plays_rec <- recipe(possession_score ~ possession_x_location + possession_y_location + score_diff + time + team_basket, data = train_data) %>%
     step_dummy(team_basket)
 
+# Determine the number of cores
 cores <- parallel::detectCores() - 1
 
+# Set random forest model to be tuned
 tune_spec <- rand_forest(mtry = tune(),
                          trees = 1000,
                          min_n = tune()) %>%
     set_mode("regression") %>%
     set_engine("ranger", num.threads = cores)
 
+# Set xgboost model to be tuned
 xgb_model <- boost_tree(trees = 1000,
                         tree_depth = tune(),
                         min_n = tune(), 
@@ -57,6 +65,8 @@ xgb_model <- boost_tree(trees = 1000,
                         learn_rate = tune()) %>%
     set_mode("regression") %>%
     set_engine("xgboost")
+
+# Set xgboost grid
 xgb_grid <- grid_latin_hypercube(
     tree_depth(),
     min_n(),
@@ -67,46 +77,59 @@ xgb_grid <- grid_latin_hypercube(
     size = 20
 )
 
+# Set xgboost workflow
 xgb_wflow <- workflow() %>%
     add_recipe(plays_rec) %>%
     add_model(xgb_model)
 
+# Set random forest workflow
 plays_wflow <- workflow() %>%
     add_recipe(plays_rec) %>%
     add_model(tune_spec)
 
+# Set cross validation folds
 folds <- vfold_cv(train_data, v = 5)
 
+# Tune the random forest
 plays_tune <- plays_wflow %>% 
     tune_grid(resamples = folds,
          grid = 25) 
 
+# Use parallel to tune xgboost
 doParallel::registerDoParallel()
 
+# Tune xgboost model
 xgb_tune <- xgb_wflow %>%
     tune_grid(resamples = folds, 
               grid = xgb_grid)
 
+# Collect RMSE for random forest
 plays_tune %>% collect_metrics(metric = "rmse")
 
+# Collect RMSE for xgboost
 xgb_tune %>% collect_metrics(metric = "rmse")
 
+# Get best tuning parameters for random forest
 plays_tune %>% select_best(metric = "rmse")
 
+# Get best tuning parameters for xgboost
 xgb_tune %>% select_best(metric = "rmse")
 
+# Split data with all data
 data_split <- initial_split(plays, prop = 3/4)
 
 # Create data frames for the two sets:
 train_data <- training(data_split)
 test_data  <- testing(data_split)
 
+# Create random forest with tuned parameters
 plays_model <- rand_forest(mtry = 2,
                            trees = 1000,
                            min_n = 30) %>%
     set_mode("regression") %>%
     set_engine("ranger", num.threads = cores)
 
+# Create xgboost with tuned parameters
 xgb_model <- boost_tree(trees = 1000,
                         tree_depth = 8,
                         min_n = 6, 
@@ -117,101 +140,74 @@ xgb_model <- boost_tree(trees = 1000,
     set_mode("regression") %>%
     set_engine("xgboost")
 
+# Workflow for random forest
 plays_wflow <- workflow() %>%
     add_recipe(plays_rec) %>%
     add_model(plays_model)
 
+# Workflow for xgboost
 xgb_wflow <- workflow() %>%
     add_recipe(plays_rec) %>%
     add_model(xgb_model)
 
+# Cross validation for random forest
 plays_fit_rs <- plays_wflow %>% 
     fit_resamples(folds)
 
+# Cross validation for xgboost
 xgb_fit_rs <- xgb_wflow %>%
     fit_resamples(folds)
 
+# Get metrics for random forest and xgboost
 collect_metrics(plays_fit_rs)
 collect_metrics(xgb_fit_rs)
 
+# Fit random forest on train data
 plays_fit <- plays_wflow %>%
     fit(train_data)
 
+# Fit xgboost on train data
 xgb_fit <- xgb_wflow %>%
     fit(train_data)
 
+# Get predictions for both modesl on the test data
 plays_testing_pred <- predict(plays_fit, test_data) %>% 
     bind_cols(test_data %>% select(possession_score)) %>%
     bind_cols(predict(xgb_fit, test_data)) %>%
     rename(RF = .pred...1,
            XGB = .pred...3)
 
+# Get RMSE for random forest
 plays_testing_pred %>% 
     rmse(possession_score, RF)
 
+# Get RMSE for xgboost
 plays_testing_pred %>% 
     rmse(possession_score, XGB)
 
+# Fit random forest and xgboost on all data
 plays_fit_all <- plays_wflow %>%
     fit(plays)
 xgb_fit_all <- xgb_wflow %>%
     fit(plays)
 
-plays_testing_pred <- predict(plays_fit_all, test_data) %>% 
-    bind_cols(test_data %>% select(possession_score)) %>%
-    bind_cols(predict(xgb_fit_all, test_data)) %>%
+# Get fitted values for all observations
+plays_testing_pred <- predict(plays_fit_all, plays) %>% 
+    bind_cols(plays %>% select(possession_score)) %>%
+    bind_cols(predict(xgb_fit_all, plays)) %>%
     rename(RF = .pred...1,
            XGB = .pred...3)
 
+# RMSE for random forest
 plays_testing_pred %>% 
     rmse(possession_score, RF)
 
+# RMSE for xgboost
 plays_testing_pred %>% 
     rmse(possession_score, XGB)
 
-plays2 <- plays %>% 
-    #dplyr::select(-X, -game_number, -possession_ids, -half) %>%
-    mutate(team_basket = factor(team_basket)) %>%
-    filter(!is.na(possession_x_location)) %>%
-    filter(!is.na(season)) %>%
-    filter(!is.na(game_coach)) %>%
-    filter(!is.na(time)) %>% 
-    filter((team_basket == "right" & possession_x_location > 550) | (team_basket == "left" & possession_x_location < 450)) %>%
-    filter(possession_score <= 3)
+# Create data for Shiny app
+plays <- plays %>%
+    bind_cols(plays_testing_pred %>% dplyr::select(RF, XGB))
 
-plot_missing(plays2)
-
-attempt <- plays2 %>% 
-    mutate(pred = plays_testing_pred$.pred) %>% 
-    group_by(timeouts_possessions) %>%
-    summarize(expected = mean(pred),
-              actual = mean(possession_score), 
-              diff = actual - expected, 
-              n = n()) #%>%
-    #inner_join(coach_info %>% dplyr::select(Coach, team_market), by = c('game_coach' = 'Coach'))
-coach_info <- read.csv("coaches_data.csv")
-
-attempt %>% arrange(desc(diff)) %>% filter(n > 100)
-attempt %>% arrange(diff) %>% filter(n > 100)
-
-plot(attempt$expected, attempt$diff)
-
-library(modelr)
-plays_grid <- plays %>% 
-    data_grid(possession_x_location = seq(min(possession_x_location), 450, length = 40),
-              possession_y_location = seq(min(possession_y_location), max(possession_y_location), length = 40),
-              score_diff = seq(-25, 25, by = 5), 
-              time = seq(max(plays$time), min(plays$time), length = 15), 
-              team_basket = "left")
-library(broom)
-grid_hat <- predict(rf, data = plays_grid)
-
-ggplot(plays_grid, aes(x = possession_x_location, y = possession_y_location)) + 
-    geom_tile(aes(fill = grid_hat$predictions), alpha = 0.7) + 
-    scale_fill_gradient("Expected Points Per Possession", breaks = c(.5, 1, 1.5, 2)) +
-    theme_minimal()
-
-right <- plays2 %>% filter(team_basket == "right")
-plot(right$possession_x_location, right$possession_y_location)
-
-grid_hat$predictions
+write.csv(plays, "shiny_data.csv")
